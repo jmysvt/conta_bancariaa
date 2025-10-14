5b0a442f2a39fea82c5bec2ceb238d4daea9c2c9
Jamily Alecrim <jmysvt@gmail.com>
2025-10-14 11:08:03 -0300

    authentication to do

---
+++ src/main/java/com/senai/conta_bancaria/Application/Infrastructure/config/AdminBootstrap.java
@@ -0,0 +1,10 @@
+package com.senai.conta_bancaria.Application.Infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminBootstrap {
+
+}
